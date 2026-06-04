595a8a43b2bbc431104d48c6dbc743c387935082
splint314 <krigal323@gmail.com>
2026-06-04 15:59:42 +0200

    fix: used dots instead underscores for order

--- sql_join/4-courses-and-students.sql
+++ sql_join/4-courses-and-students.sql
@@ -2,4 +2,4 @@ SELECT courses.title AS course_title, students.name AS student_name
 FROM courses
 LEFT JOIN enrollments ON courses.id = enrollments.course_id
 LEFT JOIN students ON students.id = enrollments.student_id
-ORDER BY courses_title ASC, students_name ASC;
+ORDER BY courses.title ASC, students.name ASC;
